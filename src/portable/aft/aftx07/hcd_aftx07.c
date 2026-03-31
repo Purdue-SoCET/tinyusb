7f6741d7b927d80cd5eb2c068a478350e55d225c
Aman <katyal0@purdue.edu>
2026-03-31 04:21:12 -0400

    aman branch

--- src/portable/aft/aftx07/hcd_aftx07.c
+++ src/portable/aft/aftx07/hcd_aftx07.c
@@ -9,6 +9,8 @@
 
 #include <stdint.h>
 
+#define MMIO_PRINTER ((volatile uint32_t*)0xb0000000)
+
 typedef struct {
   bool busy;
   uint8_t dev_addr;
@@ -19,6 +21,31 @@ typedef struct {
 } usb_xfer_ctx_t;
 
 static volatile usb_xfer_ctx_t g_xfer;
+
+// Data toggle tracking (for up to 8 devices and 16 endpoints)
+static uint16_t g_toggle_in[8];
+static uint16_t g_toggle_out[8];
+
+// Helper unions to avoid type-punning warnings
+typedef union {
+  uint32_t val;
+  usb_ctrl_bits_t bits;
+} usb_ctrl_reg_t;
+
+typedef union {
+  uint32_t val;
+  usb_status_bits_t bits;
+} usb_status_reg_t;
+
+typedef union {
+  uint32_t val;
+  usb_token_bits_t bits;
+} usb_token_reg_t;
+
+typedef union {
+  uint32_t val;
+  usb_rx_stat_bits_t bits;
+} usb_rx_stat_reg_t;
 
 //--------------------------------------------------------------------+
 // Controller API
@@ -32,20 +59,27 @@ bool hcd_configure(uint8_t rhport, uint32_t cfg_id, const void *cfg_param) {
 bool hcd_init(uint8_t rhport, const tusb_rhport_init_t *rh_init) {
   (void) rhport; (void) rh_init;
 
-  usb_ctrl_bits_t ctrl = {0};
-  ctrl.tx_flush = 1;
-  ctrl.phy_opmode = 0;
-  ctrl.phy_xcvrselect = 1;
-  ctrl.phy_termselect = 1;
-  ctrl.phy_dppulldown = 1;
-  ctrl.phy_dmpulldown = 1;
-  ctrl.enable_sof = 0;
-  USB_HW_WRITE(USB_REG_OFS_CTRL, *(uint32_t*)&ctrl);
+  usb_ctrl_reg_t ctrl;
+  ctrl.val = 0;
+  ctrl.bits.tx_flush = 1;
+  ctrl.bits.phy_opmode = 0;
+  ctrl.bits.phy_xcvrselect = 1;
+  ctrl.bits.phy_termselect = 1;
+  ctrl.bits.phy_dppulldown = 1;
+  ctrl.bits.phy_dmpulldown = 1;
+  ctrl.bits.enable_sof = 0;
+  USB_HW_WRITE(USB_REG_OFS_CTRL, ctrl.val);
 
   // Clear pending interrupts
   uint32_t irq_sts = USB_HW_READ(USB_REG_OFS_IRQ_STS);
   USB_HW_WRITE(USB_REG_OFS_IRQ_ACK, irq_sts);
   USB_HW_WRITE(USB_REG_OFS_IRQ_MASK, 0);
+
+  // Reset toggles
+  for (int i=0; i<8; i++) {
+    g_toggle_in[i] = 0;
+    g_toggle_out[i] = 0;
+  }
 
   return true;
 }
@@ -56,46 +90,50 @@ void hcd_int_handler(uint8_t rhport, bool in_isr) {
   uint32_t status = irq_sts & irq_mask;
 
   if (status & USB_IRQ_DEVICE_DETECT) {
-    uint32_t raw_status = USB_HW_READ(USB_REG_OFS_STATUS);
-    usb_status_bits_t *hw_stat = (usb_status_bits_t*)&raw_status;
+    usb_status_reg_t hw_stat;
+    hw_stat.val = USB_HW_READ(USB_REG_OFS_STATUS);
     
-    if (hw_stat->linestate != 0) {
-      // ADD THIS: Print 'A' for Attach directly to the MMIO printer
-      volatile uint32_t *printer = (uint32_t *)0xb0000000;
-      *printer = 'A';
-      *printer = '\n';
-      
+    if (hw_stat.bits.linestate != 0) {
+      *MMIO_PRINTER = 'A'; // Attach
       hcd_event_device_attach(rhport, in_isr);
     } else {
+      *MMIO_PRINTER = 'D'; // Detach
       hcd_event_device_remove(rhport, in_isr);
     }
     USB_HW_WRITE(USB_REG_OFS_IRQ_ACK, USB_IRQ_DEVICE_DETECT);
   }
 
   if (status & USB_IRQ_DONE) {
-    uint32_t rx_stat_val = USB_HW_READ(USB_REG_OFS_RX_STAT);
+    usb_rx_stat_reg_t rx_stat;
+    rx_stat.val = USB_HW_READ(USB_REG_OFS_RX_STAT);
     xfer_result_t result = XFER_RESULT_SUCCESS;
     uint32_t actual_len = 0;
 
-    if (rx_stat_val & (1u << 30)) { 
-      result = XFER_RESULT_FAILED;
-    } else if (rx_stat_val & (1u << 29)) { 
+    if (rx_stat.bits.resp_timeout) {
+      *MMIO_PRINTER = 'T'; // Timeout
+      result = XFER_RESULT_TIMEOUT;
+    } else if (rx_stat.bits.crc_err) {
+      *MMIO_PRINTER = 'C'; // CRC Error
       result = XFER_RESULT_FAILED;
     } else {
-      uint8_t resp_pid = (uint8_t)((rx_stat_val >> 16) & 0xFF);
-      if (resp_pid == 0x1E) { 
-        result = XFER_RESULT_STALLED;
+      actual_len = rx_stat.bits.count;
+      *MMIO_PRINTER = 'K'; // OK (Done)
+      
+      if (g_xfer.in && actual_len > 0) {
+        for (uint16_t i=0; i<actual_len; i++) {
+          g_xfer.buffer[i] = (uint8_t)USB_HW_READ(USB_REG_OFS_DATA);
+        }
       }
     }
 
     if (g_xfer.busy) {
-      if (g_xfer.in) {
-        actual_len = (uint32_t)(rx_stat_val & 0xFFFF);
-        for (uint32_t i = 0; i < actual_len; i++) {
-          g_xfer.buffer[i] = (uint8_t)USB_HW_READ(USB_REG_OFS_DATA);
+      if (result == XFER_RESULT_SUCCESS) {
+        // Successful transfer, update toggle
+        uint8_t ep_num = g_xfer.ep_addr & 0x0F;
+        if (g_xfer.dev_addr < 8) {
+          if (g_xfer.in) g_toggle_in[g_xfer.dev_addr] ^= (1 << ep_num);
+          else           g_toggle_out[g_xfer.dev_addr] ^= (1 << ep_num);
         }
-      } else {
-        actual_len = (result == XFER_RESULT_SUCCESS) ? g_xfer.total_len : 0;
       }
 
       hcd_event_xfer_complete(g_xfer.dev_addr, g_xfer.ep_addr, actual_len, result, in_isr);
@@ -106,10 +144,12 @@ void hcd_int_handler(uint8_t rhport, bool in_isr) {
   }
 
   if (status & USB_IRQ_ERR) {
-    uint32_t raw_ctrl = USB_HW_READ(USB_REG_OFS_CTRL);
-    usb_ctrl_bits_t *ctrl = (usb_ctrl_bits_t*)&raw_ctrl;
-    ctrl->tx_flush = 1;
-    USB_HW_WRITE(USB_REG_OFS_CTRL, *(uint32_t*)ctrl);
+    *MMIO_PRINTER = 'E'; // Error IRQ
+    
+    usb_ctrl_reg_t ctrl;
+    ctrl.val = USB_HW_READ(USB_REG_OFS_CTRL);
+    ctrl.bits.tx_flush = 1;
+    USB_HW_WRITE(USB_REG_OFS_CTRL, ctrl.val);
     
     if (g_xfer.busy) {
       hcd_event_xfer_complete(g_xfer.dev_addr, g_xfer.ep_addr, 0, XFER_RESULT_FAILED, in_isr);
@@ -125,11 +165,10 @@ void hcd_int_handler(uint8_t rhport, bool in_isr) {
 
 void hcd_int_enable(uint8_t rhport) {
   (void) rhport;
-  uint32_t usb_mask = USB_IRQ_DEVICE_DETECT | USB_IRQ_DONE | USB_IRQ_ERR | USB_IRQ_SOF;
+  uint32_t usb_mask = USB_IRQ_DEVICE_DETECT | USB_IRQ_DONE | USB_IRQ_ERR;
   USB_HW_WRITE(USB_REG_OFS_IRQ_ACK, usb_mask); 
   USB_HW_WRITE(USB_REG_OFS_IRQ_MASK, usb_mask);
 
-// Only execute PLIC interrupt config on real RISC-V hardware, skip on Verilator
 #ifndef USB_TESTBENCH
   uint32_t src = USB_PLIC_SRC;
   uint32_t ctx = 0;
@@ -144,54 +183,69 @@ void hcd_int_disable(uint8_t rhport) { (void) rhport; }
 
 uint32_t hcd_frame_number(uint8_t rhport) {
   (void) rhport;
-  uint32_t raw_status = USB_HW_READ(USB_REG_OFS_STATUS);
-  usb_status_bits_t *stat = (usb_status_bits_t*)&raw_status;
-  return stat->sof_time;
+  usb_status_reg_t stat;
+  stat.val = USB_HW_READ(USB_REG_OFS_STATUS);
+  return stat.bits.sof_time;
 }
 
 bool hcd_port_connect_status(uint8_t rhport) {
   (void) rhport;
-  uint32_t raw_status = USB_HW_READ(USB_REG_OFS_STATUS);
-  usb_status_bits_t *stat = (usb_status_bits_t*)&raw_status;
-  return (stat->linestate != 0);
+  usb_status_reg_t stat;
+  stat.val = USB_HW_READ(USB_REG_OFS_STATUS);
+  return (stat.bits.linestate != 0);
 }
 
 void hcd_port_reset(uint8_t rhport) {
   (void) rhport;
-  uint32_t raw_ctrl = USB_HW_READ(USB_REG_OFS_CTRL);
-  usb_ctrl_bits_t *ctrl = (usb_ctrl_bits_t*)&raw_ctrl;
-  ctrl->phy_opmode = 2;
-  ctrl->enable_sof = 0;
-  ctrl->phy_termselect = 0;
-  ctrl->phy_xcvrselect = 0;
-  USB_HW_WRITE(USB_REG_OFS_CTRL, *(uint32_t*)ctrl);
+  usb_ctrl_reg_t ctrl;
+  ctrl.val = USB_HW_READ(USB_REG_OFS_CTRL);
+  ctrl.bits.phy_opmode = 2;
+  ctrl.bits.enable_sof = 0;
+  ctrl.bits.phy_termselect = 0;
+  ctrl.bits.phy_xcvrselect = 0;
+  USB_HW_WRITE(USB_REG_OFS_CTRL, ctrl.val);
 }
 
 void hcd_port_reset_end(uint8_t rhport) {
   (void) rhport;
-  uint32_t raw_ctrl = USB_HW_READ(USB_REG_OFS_CTRL);
-  usb_ctrl_bits_t *ctrl = (usb_ctrl_bits_t*)&raw_ctrl;
-  ctrl->phy_opmode = 0;
-  ctrl->phy_termselect = 1;
-  ctrl->phy_xcvrselect = 1;
-  ctrl->tx_flush = 1;
-  ctrl->enable_sof = 1;
-  USB_HW_WRITE(USB_REG_OFS_CTRL, *(uint32_t*)ctrl);
+  usb_ctrl_reg_t ctrl;
+  ctrl.val = USB_HW_READ(USB_REG_OFS_CTRL);
+  ctrl.bits.phy_opmode = 0;
+  ctrl.bits.phy_termselect = 1;
+  ctrl.bits.phy_xcvrselect = 1;
+  ctrl.bits.tx_flush = 1;
+  ctrl.bits.enable_sof = 1;
+  USB_HW_WRITE(USB_REG_OFS_CTRL, ctrl.val);
 }
 
 tusb_speed_t hcd_port_speed_get(uint8_t rhport) {
   (void) rhport;
-  uint32_t raw_status = USB_HW_READ(USB_REG_OFS_STATUS);
-  usb_status_bits_t *stat = (usb_status_bits_t*)&raw_status;
-  if (stat->linestate & 0x01) return TUSB_SPEED_FULL;
-  if (stat->linestate & 0x02) return TUSB_SPEED_LOW;
+  usb_status_reg_t stat;
+  stat.val = USB_HW_READ(USB_REG_OFS_STATUS);
+  if (stat.bits.linestate & 0x01) return TUSB_SPEED_FULL;
+  if (stat.bits.linestate & 0x02) return TUSB_SPEED_LOW;
   return TUSB_SPEED_FULL;
 }
 
 void hcd_device_close(uint8_t rhport, uint8_t dev_addr) { (void) rhport; (void) dev_addr; }
 
-bool hcd_edpt_open(uint8_t rhport, uint8_t dev_addr, tusb_desc_endpoint_t const *ep_desc) { return true; }
-bool hcd_edpt_close(uint8_t rhport, uint8_t daddr, uint8_t ep_addr) { return false; }
+bool hcd_edpt_open(uint8_t rhport, uint8_t dev_addr, tusb_desc_endpoint_t const *ep_desc) { 
+  (void) rhport;
+  uint8_t const ep_num = tu_edpt_number(ep_desc->bEndpointAddress);
+  bool const in = tu_edpt_dir(ep_desc->bEndpointAddress) == TUSB_DIR_IN;
+
+  // Initialize toggle to 0 for new endpoint
+  if (dev_addr < 8) {
+    if (in) g_toggle_in[dev_addr] &= ~(1 << ep_num);
+    else    g_toggle_out[dev_addr] &= ~(1 << ep_num);
+  }
+  return true; 
+}
+
+bool hcd_edpt_close(uint8_t rhport, uint8_t daddr, uint8_t ep_addr) { 
+  (void) rhport; (void) daddr; (void) ep_addr;
+  return true; 
+}
 
 bool hcd_edpt_xfer(uint8_t rhport, uint8_t dev_addr, uint8_t ep_addr, uint8_t *buffer, uint16_t buflen) {
   (void) rhport;
@@ -200,12 +254,13 @@ bool hcd_edpt_xfer(uint8_t rhport, uint8_t dev_addr, uint8_t ep_addr, uint8_t *b
   g_xfer.total_len = buflen;
   g_xfer.in = (ep_addr & 0x80) ? true : false;
   g_xfer.busy = true;
+  g_xfer.buffer = buffer;
 
   if (!g_xfer.in && buflen > 0) {
-    uint32_t raw_ctrl = USB_HW_READ(USB_REG_OFS_CTRL);
-    usb_ctrl_bits_t *ctrl = (usb_ctrl_bits_t*)&raw_ctrl;
-    ctrl->tx_flush = 1;
-    USB_HW_WRITE(USB_REG_OFS_CTRL, *(uint32_t*)ctrl);
+    usb_ctrl_reg_t ctrl;
+    ctrl.val = USB_HW_READ(USB_REG_OFS_CTRL);
+    ctrl.bits.tx_flush = 1;
+    USB_HW_WRITE(USB_REG_OFS_CTRL, ctrl.val);
     
     for (uint16_t i = 0; i < buflen; i++) {
       USB_HW_WRITE(USB_REG_OFS_DATA, buffer[i]);
@@ -214,25 +269,33 @@ bool hcd_edpt_xfer(uint8_t rhport, uint8_t dev_addr, uint8_t ep_addr, uint8_t *b
 
   USB_HW_WRITE(USB_REG_OFS_XFER_DATA, buflen);
 
-  usb_token_bits_t token = {0};
-  token.dev_addr = dev_addr;
-  token.ep_addr = ep_addr & 0x0F;
-  token.in_xfer = g_xfer.in ? 1 : 0;
-  token.ack = 1;
-  token.pid_bits = g_xfer.in ? USB_PID_IN : USB_PID_OUT;
-  token.pid_datax = 0; // Temporarily hardcoded to 0 to pass compilation
-  token.start = 1;     
+  uint8_t ep_num = ep_addr & 0x0F;
+  uint8_t toggle = 0;
+  if (dev_addr < 8) {
+    if (g_xfer.in) toggle = (g_toggle_in[dev_addr] >> ep_num) & 0x01;
+    else           toggle = (g_toggle_out[dev_addr] >> ep_num) & 0x01;
+  }
 
-  USB_HW_WRITE(USB_REG_OFS_XFER_TOKEN, *(uint32_t*)&token);
+  usb_token_reg_t token;
+  token.val = 0;
+  token.bits.dev_addr = dev_addr;
+  token.bits.ep_addr = ep_num;
+  token.bits.in_xfer = g_xfer.in ? 1 : 0;
+  token.bits.ack = 1;
+  token.bits.pid_bits = g_xfer.in ? USB_PID_IN : USB_PID_OUT;
+  token.bits.pid_datax = toggle;
+  token.bits.start = 1;     
+
+  USB_HW_WRITE(USB_REG_OFS_XFER_TOKEN, token.val);
   return true;
 }
 
 bool hcd_edpt_abort_xfer(uint8_t rhport, uint8_t dev_addr, uint8_t ep_addr) {
   (void) rhport; (void) dev_addr; (void) ep_addr;
-  uint32_t raw_ctrl = USB_HW_READ(USB_REG_OFS_CTRL);
-  usb_ctrl_bits_t *ctrl = (usb_ctrl_bits_t*)&raw_ctrl;
-  ctrl->tx_flush = 1;
-  USB_HW_WRITE(USB_REG_OFS_CTRL, *(uint32_t*)ctrl);
+  usb_ctrl_reg_t ctrl;
+  ctrl.val = USB_HW_READ(USB_REG_OFS_CTRL);
+  ctrl.bits.tx_flush = 1;
+  USB_HW_WRITE(USB_REG_OFS_CTRL, ctrl.val);
   return true;
 }
 
@@ -243,11 +306,18 @@ bool hcd_setup_send(uint8_t rhport, uint8_t dev_addr, uint8_t const setup_packet
   g_xfer.total_len = 8;
   g_xfer.in       = false;
   g_xfer.busy     = true;
+  g_xfer.buffer   = NULL;
 
-  uint32_t raw_ctrl = USB_HW_READ(USB_REG_OFS_CTRL);
-  usb_ctrl_bits_t *ctrl = (usb_ctrl_bits_t*)&raw_ctrl;
-  ctrl->tx_flush = 1;
-  USB_HW_WRITE(USB_REG_OFS_CTRL, *(uint32_t*)ctrl);
+  // SETUP always resets toggles for EP0 to DATA1 for the next stage
+  if (dev_addr < 8) {
+    g_toggle_in[dev_addr] |= 1;  // Set next IN to DATA1
+    g_toggle_out[dev_addr] |= 1; // Set next OUT to DATA1
+  }
+
+  usb_ctrl_reg_t ctrl;
+  ctrl.val = USB_HW_READ(USB_REG_OFS_CTRL);
+  ctrl.bits.tx_flush = 1;
+  USB_HW_WRITE(USB_REG_OFS_CTRL, ctrl.val);
 
   for (int i = 0; i < 8; i++) {
     USB_HW_WRITE(USB_REG_OFS_DATA, setup_packet[i]);
@@ -255,16 +325,17 @@ bool hcd_setup_send(uint8_t rhport, uint8_t dev_addr, uint8_t const setup_packet
 
   USB_HW_WRITE(USB_REG_OFS_XFER_DATA, 8);
 
-  usb_token_bits_t token = {0};
-  token.pid_bits = USB_PID_SETUP;
-  token.dev_addr = dev_addr;
-  token.ep_addr  = 0;
-  token.pid_datax = 0; 
-  token.ack       = 1;
-  token.in_xfer   = 0;
-  token.start     = 1;
+  usb_token_reg_t token;
+  token.val = 0;
+  token.bits.pid_bits = USB_PID_SETUP;
+  token.bits.dev_addr = dev_addr;
+  token.bits.ep_addr  = 0;
+  token.bits.pid_datax = 0; // SETUP is always DATA0
+  token.bits.ack       = 1;
+  token.bits.in_xfer   = 0;
+  token.bits.start     = 1;
 
-  USB_HW_WRITE(USB_REG_OFS_XFER_TOKEN, *(uint32_t*)&token);
+  USB_HW_WRITE(USB_REG_OFS_XFER_TOKEN, token.val);
   return true;
 }
 
--- src/portable/aft/aftx07/usb_pal.h
+++ src/portable/aft/aftx07/usb_pal.h
@@ -10,88 +10,88 @@
 
 /* --- Bitfield Definitions --- */
 typedef struct {
-  uint32_t enable_sof : 1;     
-  uint32_t phy_opmode : 2;     
-  uint32_t phy_xcvrselect : 2; 
-  uint32_t phy_termselect : 1; 
-  uint32_t phy_dppulldown : 1; 
-  uint32_t phy_dmpulldown : 1; 
-  uint32_t tx_flush : 1;       
+  uint32_t enable_sof : 1;
+  uint32_t phy_opmode : 2;
+  uint32_t phy_xcvrselect : 2;
+  uint32_t phy_termselect : 1;
+  uint32_t phy_dppulldown : 1;
+  uint32_t phy_dmpulldown : 1;
+  uint32_t tx_flush : 1;
   uint32_t reserved : 23;
 } usb_ctrl_bits_t;
 
 typedef struct {
-  uint32_t linestate : 2; 
-  uint32_t rx_error : 1;  
+  uint32_t linestate : 2;
+  uint32_t rx_error : 1;
   uint32_t reserved : 13;
-  uint32_t sof_time : 16; 
+  uint32_t sof_time : 16;
 } usb_status_bits_t;
 
 typedef struct {
-  uint32_t sof : 1;           
-  uint32_t done : 1;          
-  uint32_t err : 1;           
-  uint32_t device_detect : 1; 
+  uint32_t sof : 1;
+  uint32_t done : 1;
+  uint32_t err : 1;
+  uint32_t device_detect : 1;
   uint32_t reserved : 28;
 } usb_irq_bits_t;
 
 typedef struct {
-  uint32_t count : 16;   
-  uint32_t resp_pid : 8; 
+  uint32_t count : 16;
+  uint32_t resp_pid : 8;
   uint32_t reserved : 4;
-  uint32_t idle : 1;          
-  uint32_t resp_timeout : 1;  
-  uint32_t crc_err : 1;       
-  uint32_t start_pending : 1; 
+  uint32_t idle : 1;
+  uint32_t resp_timeout : 1;
+  uint32_t crc_err : 1;
+  uint32_t start_pending : 1;
 } usb_rx_stat_bits_t;
 
 typedef struct {
   uint32_t reserved1 : 5;
-  uint32_t ep_addr : 4;  
-  uint32_t dev_addr : 7; 
-  uint32_t pid_bits : 8; 
+  uint32_t ep_addr : 4;
+  uint32_t dev_addr : 7;
+  uint32_t pid_bits : 8;
   uint32_t reserved2 : 4;
-  uint32_t pid_datax : 1; 
-  uint32_t ack : 1;       
-  uint32_t in_xfer : 1;   
-  uint32_t start : 1;     
+  uint32_t pid_datax : 1;
+  uint32_t ack : 1;
+  uint32_t in_xfer : 1;
+  uint32_t start : 1;
 } usb_token_bits_t;
 
 
 /* --- Register Offsets --- */
-#define USB_REG_OFS_CTRL       0x00
-#define USB_REG_OFS_STATUS     0x04
-#define USB_REG_OFS_IRQ_ACK    0x08
-#define USB_REG_OFS_IRQ_STS    0x0C
-#define USB_REG_OFS_IRQ_MASK   0x10
-#define USB_REG_OFS_XFER_DATA  0x14
+#define USB_REG_OFS_CTRL 0x00
+#define USB_REG_OFS_STATUS 0x04
+#define USB_REG_OFS_IRQ_ACK 0x08
+#define USB_REG_OFS_IRQ_STS 0x0C
+#define USB_REG_OFS_IRQ_MASK 0x10
+#define USB_REG_OFS_XFER_DATA 0x14
 #define USB_REG_OFS_XFER_TOKEN 0x18
-#define USB_REG_OFS_RX_STAT    0x1C
-#define USB_REG_OFS_DATA       0x20
+#define USB_REG_OFS_RX_STAT 0x1C
+#define USB_REG_OFS_DATA 0x20
 
 /* --- Hardware Access Macros --- */
 #ifdef USB_TESTBENCH
   #ifdef __cplusplus
-  extern "C" {
+extern "C" {
   #endif
-  
-  extern void usbhw_reg_write(uint32_t addr, uint32_t data);
-  extern uint32_t usbhw_reg_read(uint32_t addr);
-  
+
+extern void usbhw_reg_write(uint32_t addr, uint32_t data);
+extern uint32_t usbhw_reg_read(uint32_t addr);
+
   #ifdef __cplusplus
-  }
+}
   #endif
 
   // Directly pass the offset into Verilator
   #define USB_HW_WRITE(offset, data) usbhw_reg_write((offset), (data))
-  #define USB_HW_READ(offset)        usbhw_reg_read((offset))
+  #define USB_HW_READ(offset) usbhw_reg_read((offset))
 #else
   // Real CPU hardware address base
-  #define USB_HOST_BASE ((uint32_t)0x90003000)
+  #define USB_HOST_BASE ((uint32_t) 0x90003000)
 
   // In hardware, write to the physical memory address
-  #define USB_HW_WRITE(offset, data) (*(volatile uint32_t*)(USB_HOST_BASE + (offset)) = (data))
-  #define USB_HW_READ(offset)        (*(volatile uint32_t*)(USB_HOST_BASE + (offset)))
+  #define USB_HW_WRITE(offset, data) (*(volatile uint32_t *) (USB_HOST_BASE + (offset)) = (data))
+  #define USB_HW_READ(offset) (*(volatile uint32_t *) (USB_HOST_BASE + (offset)))
 #endif
 
 /* --- Interrupt Masks --- */
